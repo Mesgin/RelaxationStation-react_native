97f5ab77340917c6f498885fc74cd1b0efa9bf63
Mo Mesgin <mo.mesgin@gmail.com>
2018-05-18 17:45:20 -0700

    add img & button

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
+import zen from './zen.png'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>Hello Mo</Text>
+        <Text style={styles.title}>I'm ready to relax...</Text>
+        <TouchableOpacity style={styles.button}>
+          <Image source={zen} style={styles.zenImg} />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -14,12 +18,28 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#a5f0ff',
+    backgroundColor: '#f4feff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
     fontSize: 20,
-    
+    fontStyle: 'italic'
+  },
+  button: {
+    backgroundColor: 'cyan',
+    padding: 10,
+    marginBottom: 0,
+    marginTop: 20,
+    borderRadius: 100,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 3},
+    shadowOpacity: 0.30,
+    shadowRadius: 4
+
+  },
+  zenImg: {
+    width: 100,
+    height: 100
   }
 });
