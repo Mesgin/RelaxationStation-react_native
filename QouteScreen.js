6c28e1d8c03261192213add8ec2c25bdb89d0079
Mo Mesgin <mo.mesgin@gmail.com>
2018-05-21 23:37:41 -0700

    add other param

--- QouteScreen.js
+++ QouteScreen.js
@@ -4,10 +4,12 @@ import { Button, View, Text } from 'react-native';
 export default class DetailsScreen extends React.Component {
   render() {
     const param = this.props.navigation.getParam('param','no-param')
+    const otherParam = this.props.navigation.getParam('otherParam','no-other-param')
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Qoute Screen</Text>       
         <Text>Param: {JSON.stringify(param)}</Text>        
+        <Text>Other Param: {JSON.stringify(otherParam)}</Text>        
         <Button
           title="Go to Home"
           onPress={() => this.props.navigation.navigate('Home')}
