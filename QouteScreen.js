43e8de70bd796a029d5842b2ef04f9bc76264a5c
Mo Mesgin <mo.mesgin@gmail.com>
2018-05-22 22:51:19 -0700

    add image background

--- QouteScreen.js
+++ QouteScreen.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import { Button, View, Text } from 'react-native';
+import React from 'react'
+import { Button, View, Text, ImageBackground, StyleSheet } from 'react-native'
+const bg = require('./assets/img/bg.jpg')
 
 export default class QoutesScreen extends React.Component {
   render() {
-    const param = this.props.navigation.getParam('param','no-param')
-    const otherParam = this.props.navigation.getParam('otherParam','no-other-param')
+    const param = this.props.navigation.getParam('param', 'no-param')
+    const otherParam = this.props.navigation.getParam('otherParam', 'no-other-param')
+
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Qoute Screen</Text>       
-        <Text>Param: {JSON.stringify(param)}</Text>        
-        <Text>Other Param: {JSON.stringify(otherParam)}</Text>
-        <Text>{JSON.stringify(this.props.screenProps[0])}</Text>        
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-        <Button
-          title="Back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-      </View>
-    );
+      <ImageBackground source={bg} style={styles.bg}>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Qoute Screen</Text>
+          <Text>Param: {JSON.stringify(param)}</Text>
+          <Text>Other Param: {JSON.stringify(otherParam)}</Text>
+          <Text>{JSON.stringify(this.props.screenProps[0])}</Text>
+          <Button
+            title="Go to Home"
+            onPress={() => this.props.navigation.navigate('Home')}
+          />
+          <Button
+            title="Back"
+            onPress={() => this.props.navigation.goBack()}
+          />
+        </View>
+      </ImageBackground>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  bg: {
+    flex: 1,
+    resizeMode: 'cover',
+    width: undefined,
+    height: undefined,
+  }
+})
